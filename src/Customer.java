ad15b997ad7bb8bb98a3df576d78080580a3c8d2
Thomas Hammerbak <thomashammerbak@gmail.com>
2012-02-14 12:37:01 +0100

    Added base structure

---
+++ src/Customer.java
@@ -0,0 +1,66 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+
+public class Customer {
+
+	private String cName;
+	private long cBarCode;
+	private int purchaseNr;
+	private String cVectorTeam;
+	private String cCrossVectorTeam;
+	private String cStudyLine;
+	private HashMap<Integer, Purchase> purchase = new HashMap<Integer, Purchase>();
+	private Purchase newPurchase;
+	
+	public Customer(String cName, long cBarCode, String cCrossVectorTeam, String cStudyLine){
+		
+		this.cName = cName;
+		this.cBarCode = cBarCode;
+		this.cCrossVectorTeam = cCrossVectorTeam;
+		this.cStudyLine = cStudyLine;
+		purchaseNr = 0;
+		
+	}
+	
+	public String toString(){
+		return "Name: " + cName + ", Barcode: " + cBarCode + ", Studyline: " + 
+			   cStudyLine + ", Cross vector team: " + cCrossVectorTeam;
+	}
+	
+	public Purchase makePurchase(Product product, int pAmount){
+		newPurchase = new Purchase(product, pAmount);
+		purchase.put(purchaseNr, newPurchase);
+		purchaseNr++;
+		return newPurchase;
+	}
+	
+	public HashMap<Integer, Purchase> getPurchase(){
+		return (HashMap<Integer, Purchase>) purchase;
+	}
+	
+	public String getPurchaseString(){
+		String hMap = null;
+		for(int i = 0; i < purchase.size(); i++){
+			hMap = purchase.toString();
+		}
+		return hMap;
+	}
+	
+	public String getName(){
+		return cName;
+	}
+	
+	public long getBarCode(){
+		return cBarCode;
+	}
+	
+	public String getCrossVectorTeam(){
+		return cCrossVectorTeam;
+	}
+	
+	public String getStudyLine(){
+		return cStudyLine;
+	}
+	
+}
---
+++ src/Purchase.java
@@ -0,0 +1,12 @@
+
+public class Purchase {
+
+	private Product product;
+	private int pAmount;
+	
+	public Purchase(Product product, int pAmount){
+		this.product = product;
+		this.pAmount = pAmount;
+	}
+	
+}
